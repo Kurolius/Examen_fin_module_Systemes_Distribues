5304f2b8ea97b1572bfe54ffc4828a78d7ad4c82
Kurolius <ymajbar.pro@gmail.com>
2022-12-26 10:29:04 +0100

    Discovery & Gateway Service

--- common-api/src/main/java/me/majbar/events.kt
+++ common-api/src/main/java/me/majbar/events.kt
@@ -6,30 +6,30 @@ abstract class BaseEvent<T> (
 data class CustomerCreatedEvent(
     override val id : String,
     val payload:CustomerDTO
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class CustomerModifiedEvent(
     override val id : String,
     val payload:CustomerDTO
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class CustomerDeletedEvent(
     override val id : String,
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class ProductCreatedEvent(
     override val id : String,
     val payload:ProductDTO
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class ProductModifiedEvent(
     override val id : String,
     val payload:ProductDTO
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class ProductDeletedEvent(
     override val id : String,
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class ProductStatusChangedEvent(
     override val id : String,
@@ -39,45 +39,45 @@ data class ProductStatusChangedEvent(
 data class CategoryCreatedEvent(
     override val id : String,
     val payload:CategoryDTO
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class CategoryModifiedEvent(
     override val id : String,
     val payload:CategoryDTO
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class CategoryDeletedEvent(
     override val id : String,
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 
 data class OrderCreatedEvent(
     override val id : String,
     val payload:OrderDTO
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class OrderModifiedEvent(
     override val id : String,
     val payload:OrderDTO
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class OrderDeletedEvent(
     override val id : String,
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class OderLineCreatedEvent(
     override val id : String,
     val payload:OrderLineDTO
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class OderLineModifiedEvent(
     override val id : String,
     val payload:OrderLineDTO
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 data class OderLineDeletedEvent(
     override val id : String,
-):BaseCommand<String>(id);
+):BaseEvent<String>(id);
 
 
 
